a63e6384152c10689c58f87ea65395b37bbc5a6c
FA <FA@FA-PC>
2017-02-06 20:32:09 -0800

    create an exception for setter methods

---
+++ src/lab3/ReportService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3;
+
+/**
+ *
+ * @author aferit
+ */
+public class ReportService {
+     
+    
+     public void doOutput(String msg){
+         System.out.println(msg);
+     }
+}
